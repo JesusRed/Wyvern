c9c8ed4765a4021f700f326e12097caf2d74cc2c
JesusRed <jesusalfredorv@ufps.edu.co>
2021-01-12 18:01:51 -0500

    initial commit

---
+++ WyvernCritics/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from WyvernCritics import views
+
+urlpatterns = [
+    path('', views.home, name="Home"),
+    path('posters', views.posters, name="Posters"),
+    path('critics', views.critics, name="Critics"),
+    path('forum', views.forum, name="Forum"),
+    path('contact', views.contact, name="Contact"),
+]
---
+++ WyvernCritics/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+
+def posters(request):
+    return render(request, "posters.html")
+
+
+def critics(request):
+    return render(request, "critics.html")
+
+
+def forum(request):
+    return render(request, "forum.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
