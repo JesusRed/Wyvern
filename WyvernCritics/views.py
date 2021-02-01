fdc1f5db5e13bbe30001ac006f3e48835d035e51
JesusRed <jesusalfredorv@ufps.edu.co>
2021-02-01 16:40:05 -0500

    filter by director on critics

--- WyvernCritics/views.py
+++ WyvernCritics/views.py
@@ -1,8 +1,11 @@
+from django import template
 from django.shortcuts import render
-from WyvernCritics.models import Review
+from WyvernCritics.models import Review, Person, Role
 
 
 # Create your views here.
 def critics(request):
+    role = Role.objects.all()
+    person = Person.objects.filter(role__tipo='Director')
     critics = Review.objects.all()
-    return render(request, "critics.html", {"critics": critics})
+    return render(request, "critics.html", {"critics": critics, "person": person, "role": role})
