ea46ddb51c9eb7a7bd8a787042acf67c36551b0c
JesusRed <jesusalfredorv@ufps.edu.co>
2021-02-02 14:58:28 -0500

    form formatted

--- WyvernContact/forms.py
+++ WyvernContact/forms.py
@@ -2,8 +2,23 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
-    name = forms.CharField(label="Name", required=True)
-    subject = forms.CharField(label="Subject", widget=forms.Select)
-    email = forms.CharField(label="Email", required=True)
-    message = forms.CharField(label="Message", required=True, widget=forms.Textarea)
+    name = forms.CharField(label="Name", required=True, widget=forms.TextInput(
+        attrs={
+            'class': 'input', 'type': 'text', 'placeholder': 'Text input'
+        }
+    ))
+
+    CHOICES = (('Asesoria', 'Asesoria'), ('Ventas', 'Ventas'))
+    subject = forms.ChoiceField(label="Subject", choices=CHOICES, widget=forms.Select())
+
+    email = forms.CharField(label="Email", required=True, widget=forms.EmailInput(
+        attrs={
+            'class': 'input is-black', 'type': 'email', 'placeholder': 'user@mail.xx'
+        }
+    ))
+
+    message = forms.CharField(label="Message", required=True, widget=forms.Textarea(
+        attrs={
+            'class': 'textarea', 'placeholder': 'Type your message', 'rows': '5'
+        }
+    ))
