32bac0533f4eb0455a4d3d5992d9f213567ab0c0
JesusRed <jesusalfredorv@ufps.edu.co>
2021-02-10 22:05:02 -0500

    models forum

--- WyvernForum/admin.py
+++ WyvernForum/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User, PostForum, ReplyForum
+
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+class ReplyAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(User, UserAdmin)
+admin.site.register(PostForum, PostAdmin)
+admin.site.register(ReplyForum, ReplyAdmin)
