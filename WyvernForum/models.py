f825e409c4a6316b3a1e7d66473a060c24a07130
JesusRed <jesusalfredorv@ufps.edu.co>
2021-02-10 21:47:17 -0500

    models forum

--- WyvernForum/models.py
+++ WyvernForum/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    profilepic = models.ImageField(upload_to='profilepic')
+    username = models.CharField(max_length=50)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    def __str__(self):
+        return self.username
+
+
+class PostForum(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    message = models.CharField(max_length=500)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "post"
+        verbose_name_plural = "posts"
+
+    def __str__(self):
+        return self.title
+
+
+class ReplyForum(models.Model):
+    post = models.ForeignKey(PostForum, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=500)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "reply"
+        verbose_name_plural = "replies"
+
+    def __str__(self):
+        return self.message
